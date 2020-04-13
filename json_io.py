8567b33f8fd5ce5ed02aac6e2d8f4db2932a9015
邹佳辰 <zoujiachen@megvii.com>
2020-04-13 11:56:34 +0800

    +feat: add json_io.py

---
+++ json_io.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+
+@File   : json_io.py   
+@Author : alexander.here@gmail.com
+@Date   : 2020-04-13 11:47 CST(+0800)   
+@Brief  :  
+
+'''
+
+import json
+
+data = {
+    'STR': 'this is a string',
+    'integer': 123,
+    'float': 456.789,
+    'boolean' : True,
+    'list' : [ i for i in range( 10)],
+    'dict' : { 1: 2, 3: 4, 5: 6},
+}
+
+# IO:
+encoded = json.dumps( data)
+loaded = json.loads( encoded)
+
+# Print:
+print( json.dumps( loaded, indent = 16, sort_keys = True))
+
+# End of 'json_io.py' 
+
