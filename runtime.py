6e515bdce84110a2191f527d5f1ab5b298375cce
邹佳辰 <zoujiachen@megvii.com>
2020-05-12 19:09:08 +0800

    +feat: runtime.py: import code fragment from path

--- runtime.py
+++ runtime.py
@@ -30,5 +30,21 @@ patch_ver = sys.version_info[ 2]
 for idx, arg in enumerate( sys.argv):
     print( 'argument #%d: %s' % (idx, arg))
 
+# import from path:
+path = 'strings.py'
+module_name = 'strings'
+if sys.version_info.major <= 2:
+    import imp
+    module = imp.load_source( module_name, path)
+elif sys.version_info.minor <= 4:
+    from importlib.machinery import SourceFileLoader
+    module = SourceFileLoader( module_name, path)
+else:
+    import importlib.util
+    spec = importlib.util.spec_from_file_location( module_name, path)
+    module = importlib.util.module_from_spec( spec)
+    spec.loader.exec_module( module)
+print( module.long_string)
+
 # End of 'runtime.py' 
 
